b7bd70b64b18935ad6ee770b51b7d55374b9ead2
Saurabh Chakravarty <saurabc@yahoo.com>
2015-11-23 13:48:32 -0500

    Added the linear weight based strategy.

--- Source/getSlidingWindowAverageModel.m
+++ Source/getSlidingWindowAverageModel.m
@@ -3,13 +3,19 @@ function model = getSlidingWindowAverageModel(trainData, windowSize, windowIncre
 
 threshold = max(trainData(:,1));
 n = length(trainData(:,1));
+weightVector = ones(windowSize,1);
+
+for i = 1 : windowSize
+	weightVector(i) = i;
+end
 
 globalAverage = 0;
 for slidingWindowDataIndex = 1 : windowIncrement : n - windowSize
     %Pick the windowSize worth of data
-    slidingWindowData = trainData(slidingWindowDataIndex:slidingWindowDataIndex + windowSize,:);
+    slidingWindowData = trainData(slidingWindowDataIndex:slidingWindowDataIndex + windowSize - 1,:);
     %Average the value in a variable
-    %Calculate the new average
+    %Calculate the new weighted average
+    slidingWindowData(:,1) = slidingWindowData(:,1) .* weightVector;
     currentAverage = mean(slidingWindowData(:,1));
     %if the currentPoint is an anomaly, set threshold to
     %abs(newAverage - oldAverage) if the difference is less
